41b73eda47a6cacccf2569ea26f54cc87a484b8b
nave <federiconaveira@gmail.com>
2022-05-15 14:03:48 +0200

    Fix issue when 2nd departure is not ready

--- src/getSlTimeHandler.js
+++ src/getSlTimeHandler.js
@@ -4,6 +4,19 @@ const axios = require('axios')
 
 const SL_APP_URL = 'https://fathomless-plains-65743.herokuapp.com/api/sl?transportType=Tram&direction=2&originId=9248'
 
+const getText = (departure1, departure2) => {
+  const resp = []
+  if (departure1) {
+    resp.push(`First in: ${departure1.time.displayTime} from ${departure1.stopAreaName} towards ${departure1.destination}.
+    `)
+  }
+  if (departure2) {
+    resp.push(`Second in: ${departure2.time.displayTime} from ${departure2.stopAreaName} towards ${departure2.destination}
+    `)
+  }
+  return resp.join('')
+}
+
 const getSlTimeHandler = async (conv) => {
   try {
     const resp = await axios.get(SL_APP_URL)
@@ -14,9 +27,8 @@ const getSlTimeHandler = async (conv) => {
     }
 
     const [firstDeparture, secondDeparture, ...others]  = resp.data
-    conv.add(`First in: ${firstDeparture.time.displayTime} from ${firstDeparture.stopAreaName} towards ${firstDeparture.destination}.
-    Second in: ${secondDeparture.time.displayTime} from ${secondDeparture.stopAreaName} towards ${secondDeparture.destination}
-    `)
+    const text = getText(firstDeparture, secondDeparture)
+    conv.add(text)
   } catch(e) {
     console.error('Error', e)
     conv.add('Oops, there were an error!')
